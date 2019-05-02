5dc258508a32258ff89984d9ca0fcad196e36ead
Ramesh Kuruba <ramesh.kuruba@ge.com>
2019-05-01 20:38:37 -0400

    Half complete

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 
-
+class Food{
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+    console.log(`Eating ${this.name}`);
+  }
+}
 class Animal {
-  constructor(name, energy, food) {
+  constructor(name, energy) {
     this.name = name
     this.energy = energy
-    this.food = food
-
   }
-  eat(value) {
-    console.log(`${this.name} is eating.`)
+  eat(name, value) {
     this.energy += value
+    console.log(`${this.name} is eating. Tiger's energy is ${this.energy}`)
   }
   sleep(value) {
-    console.log(`${this.name} is sleeping.`)
     this.energy += value
+    console.log(`${this.name} is sleeping. Energy level is ${this.energy} `)
   }
   sound(value) {
     console.log(`${this.name} is making noise.`)
@@ -22,18 +26,14 @@ class Animal {
 }
 
 class Tiger extends Animal {
-  constructor(name, energy,food) {
-    super(name, energy, food)
-  }
-  bark() {
-    console.log('Woof Woof!')
-    this.energy -= .1
+  constructor(name, energy, food) {
+    super(name, energy,food)
   }
 }
 
 class Monkey extends Animal {
-  constructor(name, energy, food) {
-    super(name, energy, food)
+  constructor(name, energy) {
+    super(name, energy)
   }
   play() {
     console.log('Oooo Oooo Oooo!')
@@ -42,12 +42,19 @@ class Monkey extends Animal {
 }
 
 class Snake extends Animal {
-  constructor(name, energy,food) {
-    super(name, energy,food)
+  constructor(name, energy) {
+    super(name, energy)
   }
 }
+const tiger1 = new Tiger('Tiger1', 20);
+const tiger2 = new Tiger('Tiger2', 20);
+const tiger3 = new Tiger('Tiger3', 20);
+tiger3.sleep(5);
+const bugs1 = new Food('cricket');
+const grain1 = new Food('wheat');
 
-const monkey1 = new Monkey('Rocky', 10, 'Wheat' );
-monkey1.play();
+tiger1.eat(grain1, 5);
+tiger2.eat(bugs1, 5);
+console.log(Tiger.instances);
 
-console.log(monkey1);
+
